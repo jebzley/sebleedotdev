9d335a2e3d91042ae1bb76a693102281daa609d2
jebzley <sebastian.ma.lee@gmail.com>
2024-03-29 09:40:33 +0000

    aria labelling

--- src/components/SocialIcons/index.tsx
+++ src/components/SocialIcons/index.tsx
@@ -8,13 +8,15 @@ export function SocialIcons() {
           href="https:/www.github.com/jebzley"
           rel="noopener noreferrer"
           target="_blank"
+          aria-label="Github (Opens in a new tab)"
         >
           <Image
             src={"/github.svg"}
             width={20}
             height={20}
-            alt="Github"
+            alt=""
             className="hover:opacity-50 transition-opacity"
+            aria-hidden="true"
           />
         </a>
       </li>
@@ -23,13 +25,15 @@ export function SocialIcons() {
           href="https://www.linkedin.com/in/sebleedev"
           rel="noopener noreferrer"
           target="_blank"
+          aria-label="Linked In (Opens in a new tab)"
         >
           <Image
             src={"/linkedin.svg"}
             width={20}
             height={20}
-            alt="Linked In"
+            alt=""
             className="hover:opacity-50 transition-opacity"
+            aria-hidden="true"
           />
         </a>
       </li>
@@ -38,12 +42,14 @@ export function SocialIcons() {
           href="https:/www.twitter.com/sebleedev"
           rel="noopener noreferrer"
           target="_blank"
+          aria-label="Twitter (Opens in a new tab)"
         >
           <Image
             src={"/twitter.svg"}
             width={20}
             height={20}
-            alt="Twitter"
+            alt=""
+            aria-hidden="true"
             className="hover:opacity-50 transition-opacity"
           />
         </a>
