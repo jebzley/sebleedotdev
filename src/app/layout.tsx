77687556bb6d62558d83204382db04b1115b506e
jebzley <sebastian.ma.lee@gmail.com>
2024-04-12 17:09:10 +0100

    hide nav based on env var

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isSmolModeEnabled = process.env.NEXT_PUBLIC_SMOL_MODE;
+
   return (
     <html lang="en">
       <body
         className={`${inter.className} p-8 lg:px-24 max-w-screen-xl mx-auto h-screen`}
       >
-        <header>
-          <Navigation />
-        </header>
+        {!isSmolModeEnabled && (
+          <header>
+            <Navigation />
+          </header>
+        )}
         <main className="h-full pt-16">{children}</main>
       </body>
     </html>
