64fde1bd7bd0a4570eae04e6aa3598973b881d39
jebzley <sebastian.ma.lee@gmail.com>
2024-05-05 15:21:05 +0100

    setup page layout

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,13 +1,39 @@
 import { redirect } from "next/navigation";
 import { PROJECT_DATA } from "@/constants/projects";
 import { ROUTES } from "@/constants/routes";
-
-const paths = PROJECT_DATA.map((project) => project.id);
+import { PageLayout } from "@/components/PageLayout";
+import { PageTitle } from "@/components/PageTitle";
+import Image from "next/image";
+import { Chip } from "@/components/Chip";
 
 export default function ProjectPage({ params }: { params: { slug: string } }) {
-  if (!paths.includes(params.slug)) {
+  const project = PROJECT_DATA.find((project) => project.id === params.slug);
+
+  if (!project) {
     redirect(ROUTES.HOME);
   }
 
-  return <p>{params.slug}</p>;
+  return (
+    <PageLayout>
+      <div className="flex flex-col gap-8">
+        <PageTitle title={project.title} subtitle={project.subtitle} />
+        {project.tools && (
+          <ul className="flex gap-4 flex-wrap" aria-label="Technologies used">
+            {project.tools.map((tool) => {
+              return (
+                <li key={tool}>
+                  <Chip>{tool}</Chip>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        <Image src={project.imgSrc} alt="" height={400} width={400} />
+      </div>
+      <div
+        className="md:overflow-scroll flex flex-col gap-4"
+        dangerouslySetInnerHTML={{ __html: project.body ?? "" }}
+      />
+    </PageLayout>
+  );
 }
